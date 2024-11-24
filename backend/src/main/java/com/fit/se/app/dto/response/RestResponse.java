e79f72873d096e6ef7ffbf3213211306caee5b74
Vu Tran <21088201.vu@student.iuh.edu.vn>
2024-11-24 23:26:31 +0700

    fix: modifing main html page

--- backend/src/main/java/com/fit/se/app/dto/response/RestResponse.java
+++ backend/src/main/java/com/fit/se/app/dto/response/RestResponse.java
@@ -1,14 +1,16 @@
 package com.fit.se.app.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Setter
-@Getter
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RestResponse<T> {
     private int statusCode;
     private String error;
-
     private Object message;
     private T data;
 }
