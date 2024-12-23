b86f3ff31c6d54829b664e82a0fbeb3386876980
Vu Tran <21088201.vu@student.iuh.edu.vn>
2024-12-23 20:42:42 +0700

    changed vite config for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   plugins: [react(), mkcert()],
@@ -9,6 +9,13 @@ export default defineConfig({
     port: 3000,
   },
   optimizeDeps: {
-    include: ['swiper/react', 'swiper'],
+    include: ["swiper/react", "swiper"],
   },
-})
+  build: {
+    outDir: "dist",
+  },
+  preview: {
+    port: 3000,
+  },
+  base: "./",
+});
