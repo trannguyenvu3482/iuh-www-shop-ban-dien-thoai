2f352a350735b4ca4c5fcf8f15560e1eff1bca8d
Vu Tran <21088201.vu@student.iuh.edu.vn>
2024-12-23 21:35:18 +0700

    fixing cors on production, again again again

--- backend/src/main/java/com/fit/se/app/config/SecurityConfiguration.java
+++ backend/src/main/java/com/fit/se/app/config/SecurityConfiguration.java
@@ -88,6 +88,7 @@ public class SecurityConfiguration {
                     configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3000", "https://localhost:8080", "https://iuh-www-shop-ban-dien-thoai.vercel.app"));
                     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
+                    configuration.setAllowCredentials(true);
                     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
                     source.registerCorsConfiguration("/**", configuration);
                     httpSecurityCorsConfigurer.configurationSource(source);
